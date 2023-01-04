dfa06e08ae8c6173ec2d8336dc8e193eb46242f6
iahirschhuber <iahirschhuber@studentshtlinnacat.onmicrosoft.com>
2023-01-04 15:13:23 +0100

    first commit

---
+++ src/main/java/sepls/seplsunderpressure/SEPLSUnderPressure.java
@@ -0,0 +1,9 @@
+package sepls.seplsunderpressure;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(SEPLSUnderPressure.MODID)
+
+public class SEPLSUnderPressure {
+    public static final String  MODID= "seplsunderpressure";
+}
