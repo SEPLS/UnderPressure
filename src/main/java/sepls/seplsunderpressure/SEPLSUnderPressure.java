a2af48a3044e399ad526846e85d45bb3df644870
iahirschhuber <iahirschhuber@studentshtlinnacat.onmicrosoft.com>
2023-01-04 16:45:24 +0100

    added tin_ingot

--- src/main/java/sepls/seplsunderpressure/SEPLSUnderPressure.java
+++ src/main/java/sepls/seplsunderpressure/SEPLSUnderPressure.java
@@ -1,9 +1,17 @@
 package sepls.seplsunderpressure;
 
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import sepls.seplsunderpressure.init.ItemInit;
 
 @Mod(SEPLSUnderPressure.MODID)
 
 public class SEPLSUnderPressure {
     public static final String  MODID= "seplsunderpressure";
+
+     public SEPLSUnderPressure () {
+         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+         ItemInit.ITEMS.register(bus);
+     }
 }
---
+++ src/main/java/sepls/seplsunderpressure/init/ItemInit.java
@@ -0,0 +1,14 @@
+package sepls.seplsunderpressure.init;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import sepls.seplsunderpressure.SEPLSUnderPressure;
+
+public class ItemInit {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, SEPLSUnderPressure.MODID);
+
+    public static final RegistryObject<Item> TIN_INGOT = ITEMS.register("tin_ingot", () -> new Item(new Item.Properties()));
+}
