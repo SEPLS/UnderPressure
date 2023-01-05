b3b3918dece53c4439ed3e98696fabac51cb7e5d
iahirschhuber <iahirschhuber@studentshtlinnacat.onmicrosoft.com>
2023-01-06 00:32:53 +0100

    added ore geb

--- src/main/java/sepls/seplsunderpressure/SEPLSUnderPressure.java
+++ src/main/java/sepls/seplsunderpressure/SEPLSUnderPressure.java
@@ -4,7 +4,9 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import sepls.seplsunderpressure.init.BlockInit;
+import sepls.seplsunderpressure.init.ConfiguredFeaturesInit;
 import sepls.seplsunderpressure.init.ItemInit;
+import sepls.seplsunderpressure.init.PlacedFeatureInit;
 
 @Mod(SEPLSUnderPressure.MODID)
 
@@ -16,5 +18,7 @@ public class SEPLSUnderPressure {
 
         BlockInit.BLOCKS.register(bus);
         ItemInit.ITEMS.register(bus);
+        ConfiguredFeaturesInit.CONFIGURED_FEATURES.register(bus);
+        PlacedFeatureInit.PLACED_FEATURES.register(bus);
     }
 }
---
+++ src/main/java/sepls/seplsunderpressure/init/ConfiguredFeaturesInit.java
@@ -0,0 +1,28 @@
+package sepls.seplsunderpressure.init;
+
+import com.google.common.base.Suppliers;
+import net.minecraft.core.Registry;
+import net.minecraft.data.worldgen.features.OreFeatures;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.OreFeature;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import sepls.seplsunderpressure.SEPLSUnderPressure;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+public class ConfiguredFeaturesInit {
+    public static final DeferredRegister <ConfiguredFeature<?, ?>>  CONFIGURED_FEATURES =
+            DeferredRegister.create(Registry.CONFIGURED_FEATURE_REGISTRY, SEPLSUnderPressure.MODID);
+
+    private static final Supplier<List<OreConfiguration.TargetBlockState>> TIN_ORE_OVERWORLD_REPLACEMENT = Suppliers.memoize(() ->
+            List.of(OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, BlockInit.TIN_ORE.get().defaultBlockState()),
+                    OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, BlockInit.TIN_ORE.get().defaultBlockState())));
+
+    public static final RegistryObject<ConfiguredFeature<?,?>> TIN_ORE = CONFIGURED_FEATURES.register("tin_ore" ,
+            () -> new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(TIN_ORE_OVERWORLD_REPLACEMENT.get(), 11)));
+
+}
