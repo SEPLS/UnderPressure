bbebeb8a51a3ae462d716af962c73c9243ec8118
iahirschhuber <iahirschhuber@studentshtlinnacat.onmicrosoft.com>
2023-01-07 21:35:44 +0100

    tried json based ore gen, removed old ore gen

--- src/main/java/sepls/seplsunderpressure/SEPLSUnderPressure.java
+++ src/main/java/sepls/seplsunderpressure/SEPLSUnderPressure.java
@@ -4,9 +4,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import sepls.seplsunderpressure.init.BlockInit;
-import sepls.seplsunderpressure.init.ConfiguredFeatureInit;
 import sepls.seplsunderpressure.init.ItemInit;
-import sepls.seplsunderpressure.init.PlacedFeatureInit;
 
 @Mod(SEPLSUnderPressure.MODID)
 
@@ -18,7 +16,6 @@ public class SEPLSUnderPressure {
 
         BlockInit.BLOCKS.register(bus);
         ItemInit.ITEMS.register(bus);
-        ConfiguredFeatureInit.CONFIGURED_FEATURES.register(bus);
-        PlacedFeatureInit.PLACED_FEATURES.register(bus);
+
     }
 }
